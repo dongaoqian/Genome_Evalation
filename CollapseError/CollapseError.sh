2f9f060960cdd3ea9409299ea28acd7d0efc6bb0
dongaoqian <145655565+dongaoqian@users.noreply.github.com>
2025-05-29 17:05:43 +0800

    Update CollapseError.sh

--- CollapseError/CollapseError.sh
+++ CollapseError/CollapseError.sh
@@ -1,7 +1,7 @@
-mosdepth -t 64 -b 50000 -n -f $1 mapping_50k $2
+mosdepth -t 64 -b 50000 -n -f $1 50k $2
 
 gzip -d 50k.regions.bed.gz
 
-python CalculateCollapseRatio.py YZ.ngs_50k.regions.bed
+python CalculateCollapseRatio.py 50k.regions.bed
 
 
